8060eddd1549b943104c092b8bb0a1251d76f5f2
phucttgiakiem <94423099+Phucttgiakiem@users.noreply.github.com>
2025-02-26 07:46:14 +0700

    Delete FE_Tiktok directory

--- FE_Tiktok/src/layouts/index.js
+++
@@ -1,3 +0,0 @@
-export { default,default as DefaultLayout } from './DefaultLayout';
-export { default as HeaderOnly } from './HeaderOnly';
-export { default as CommentLayout } from './CommentLayout';
